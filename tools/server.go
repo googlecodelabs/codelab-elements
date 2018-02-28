53a2536bf62451e900ebe5612638d8a043d0d33e
Alex Vaghin <ddos@google.com>
2018-02-28 04:15:56 -0500

    tools: move to where they belong

--- tools/server.go
+++ tools/server.go
@@ -1,11 +1,11 @@
 // Copyright 2018 Google Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,16 +15,21 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
 )
 
+var addr = flag.String("addr", "localhost:8080", "Server address to bind to")
+
 func main() {
+	flag.Parse()
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("serving from %q on http://localhost:8080", dir)
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir(dir))))
+	h := http.FileServer(http.Dir(dir))
+	log.Printf("serving from %q on http://%s", dir, *addr)
+	log.Fatal(http.ListenAndServe(*addr, h))
 }
