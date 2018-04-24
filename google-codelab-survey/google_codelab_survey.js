cbba7483561dbc0baf8e5d11d1445542da51691b
arkassay <arkassay@gmail.com>
2018-04-24 18:51:36 -0400

    Updates html format for survey, adds localstorage of results, and adds persistent state of survey answers on refresh.

--- google-codelab-survey/google_codelab_survey.js
+++ google-codelab-survey/google_codelab_survey.js
@@ -16,15 +16,29 @@
 
 goog.module('googlecodelabs.CodelabSurvey');
 
+const EventHandler = goog.require('goog.events.EventHandler');
+const HTML5LocalStorage =
+    goog.require('goog.storage.mechanism.HTML5LocalStorage');
 const Templates = goog.require('googlecodelabs.CodelabSurvey.Templates');
 const dom = goog.require('goog.dom');
+const events = goog.require('goog.events');
 const soy = goog.require('goog.soy');
 
 
 /**
- * The prefix for all survey keys in local storage
+ * The prefix for all survey keys in local storage.
+ * @const {string}
  */
 const STORAGE_KEY_PREFIX = 'codelab-survey-';
+
+/**
+ * The id for the current survey.
+ * @const {string}
+ */
+const SURVEY_ID_ATTR = 'survey-id';
+
+/** @const {string} */
+const DEFAULT_SURVEY_NAME = 'default-codelabs-survey';
 
 
 /**
@@ -39,7 +53,19 @@ class CodelabSurvey extends HTMLElement {
      * The name of the survey
      * @private {string}
      */
-    this.surveyName_ = this.getAttribute('survey-id') || '';
+    this.surveyName_ = this.getAttribute(SURVEY_ID_ATTR) || DEFAULT_SURVEY_NAME;
+
+    /** @private {!HTML5LocalStorage} */
+    this.storage_ = new HTML5LocalStorage();
+
+    /** @private {string} */
+    this.storageKey_ = STORAGE_KEY_PREFIX + this.surveyName_;
+
+    /** @private {!Object<string, !Object>} */
+    this.storedData_ = {};
+
+    /** @private {!EventHandler} */
+    this.eventHandler_ = new EventHandler();
   }
 
   /**
@@ -47,25 +73,85 @@ class CodelabSurvey extends HTMLElement {
    * @override
    */
   connectedCallback() {
+    this.checkStoredData_();
     this.updateDom_();
+    this.bindEvents_();
+  }
+
+  bindEvents_() {
+    const surveyQuestions = document.querySelectorAll('.mdc-radio__wrapper');
+    surveyQuestions.forEach(el => {
+      this.eventHandler_.listen(el, events.EventType.CLICK, (e) => {
+        this.handleSurveyClick_(e.currentTarget);
+      });
+    });
+  }
+
+  handleSurveyClick_(surveyQuestionEl) {
+    const labelEl = surveyQuestionEl.querySelector('label');
+    const answer = labelEl.textContent;
+    const question = surveyQuestionEl.querySelector('input').name;
+
+    if (question) {
+      this.storedData_[this.surveyName_][question] = answer;
+      this.storage_.set(
+        this.storageKey_, JSON.stringify(this.storedData_[this.surveyName_]));
+    }
+  }
+
+  checkStoredData_() {
+    const storedData = this.storage_.get(this.storageKey_);
+    if (storedData) {
+      this.storedData_[this.surveyName_] = /** @type {!Object} */ (
+        JSON.parse(storedData));
+    }
   }
 
   updateDom_() {
-    const paperRadioGroupEl = this.querySelector('paper-radio-group');
-    if (paperRadioGroupEl) {
-      const polymerRadioEls = paperRadioGroupEl.querySelectorAll(
-        'paper-radio-button');
-      dom.removeNode(paperRadioGroupEl);
-      polymerRadioEls.forEach(radioEl => {
-        const title = radioEl.textContent;
-        const updatedRadioEl = soy.renderAsElement(Templates.radioButton, {
-          radioGroupName: this.surveyName_,
-          radioId: title.replace(/\s+/g, '-').toLowerCase(),
-          radioTitle: title
+    const radioGroupEls = this.querySelectorAll('paper-radio-group');
+    const questionEls = this.querySelectorAll('h4');
+    const surveyQuestions = [];
+    if (radioGroupEls.length && (questionEls.length == radioGroupEls.length)) {
+      radioGroupEls.forEach((radioGroupEl, index) => {
+        const surveyOptions = [];
+        const polymerRadioEls = radioGroupEl.querySelectorAll(
+          'paper-radio-button');
+        dom.removeNode(radioGroupEl);
+        polymerRadioEls.forEach(radioEl => {
+          const title = radioEl.textContent;
+          surveyOptions.push({
+            radioId: this.normalizeIdField_(title),
+            radioTitle: title
+          });
         });
-        this.appendChild(updatedRadioEl);
+        surveyQuestions.push({
+          question: questionEls[index].textContent,
+          options: surveyOptions
+        });
+        dom.removeNode(questionEls[index]);
+      });
+      const updatedDom = soy.renderAsElement(Templates.survey, {
+        surveyName: this.surveyName_,
+        surveyQuestions: surveyQuestions
+      });
+      this.appendChild(updatedDom);
+    }
+    this.setAnsweredQuestions_();
+  }
+
+  setAnsweredQuestions_() {
+    const surveyData = this.storedData_[this.surveyName_];
+    if (surveyData) {
+      Object.keys(surveyData).forEach(key => {
+        const id = this.normalizeIdField_(surveyData[key]);
+        const inp = this.querySelector(`[id="${id}"]`);
+        if (inp) inp.checked = true;
       });
     }
+  }
+
+  normalizeIdField_(id) {
+    return id.replace(/\s+/g, '-').toLowerCase();
   }
 }
 
